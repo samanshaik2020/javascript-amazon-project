9e929511126d36c7781d3e80de8daee2a7666976
samanshaik2020 <samanshaik2020@gmail.com>
2024-09-04 16:57:44 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,10 +4,8 @@ import { formatCurrency } from '../utils/money.js';
 import { hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-hello(); 
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from './paymentSummary.js';
+
 
 export function renderOrderSummary(){
 
@@ -103,6 +101,8 @@ export function renderOrderSummary(){
 
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
+
+            renderOrderSummary();
         });
     });
 
@@ -111,6 +111,7 @@ export function renderOrderSummary(){
             const {productId, deliveryOptionId} = element.dataset; 
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
 }
