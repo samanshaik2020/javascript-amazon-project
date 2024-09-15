e460f60da633d0abaa6408602114629cc14db306
samanshaik2020 <samanshaik2020@gmail.com>
2024-09-15 15:29:49 +0530

    Rename tests-jasmine folder to tests

--- tests-jasmine/moneyTest.js
+++
@@ -1,9 +0,0 @@
-import {formatCurrency} from '../scripts/utils/money.js';
-
-describe('test suite: formatCurrency', () => {
-    it('converts cents into dollars', () => {
-        expect(formatCurrency(2095)).toEqual('20.95');
-    });
-
-    it('works with 0', () => {
-});
